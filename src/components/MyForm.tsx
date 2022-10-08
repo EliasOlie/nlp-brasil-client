1e51b44c44a60894cfb4f0cbb891c1223ac4c494
EliasOlie <eliasvieira31@gmail.com>
2022-10-08 04:13:13 -0300

    resolvido error de não setar o payload das frases

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,59 +1,54 @@
-import { useRef, useState } from 'react'
-import { SubmitHandler, FormHandles } from '@unform/core'
-import Input from './input'
-import api from '../config/API'
-import IForm from '../interfaces/IFormPhrase'
-import IPhrase from '../interfaces/IPhrase'
-import PhraseFormStyle from './styles/PhraseFormStyle'
-import ButtonHolderStyle from './styles/BtnWrapper'
-import PhraseReportStyle from './styles/PhraseReportStyle'
-import Button from './Button'
-import ShowReport from './ShowReports'
-import Show from './ShowStatistics'
-import Wrapper from './styles/formWrapper'
-import ShowPhrase from './ShowPhrase'
+import { useRef, useState } from "react";
+import { SubmitHandler, FormHandles } from "@unform/core";
+import Input from "./input";
+import api from "../config/API";
+import IForm from "../interfaces/IFormPhrase";
+import IPhrase from "../interfaces/IPhrase";
+import PhraseFormStyle from "./styles/PhraseFormStyle";
+import ButtonHolderStyle from "./styles/BtnWrapper";
+import PhraseReportStyle from "./styles/PhraseReportStyle";
+import Button from "./Button";
+import ShowReport from "./ShowReports";
+import Show from "./ShowStatistics";
+import Wrapper from "./styles/formWrapper";
+import ShowPhrase from "./ShowPhrase";
 import "dotenv/config";
 
-
 export default function MyForm() {
-	const [phrase, setPhrase] = useState<IPhrase>()
-	const [hidden, setHidden] = useState(false)
-	const formRef = useRef<FormHandles>(null)
-	const handleSubmit: SubmitHandler<IForm> = async data => {
-		await api.post(
-			'/phrase/polarity',
-			data
-		).then((Response) => {
-		
-			setPhrase(Response.data.process);
-		})
-	
-	}
-	
-	return(
-		<>
-			<Wrapper>
-				<PhraseFormStyle ref={formRef} onSubmit={handleSubmit}>
-					<Input name="phrase" placeholder="Frase"/>
-						<Button type="submit" onClick={() => {
-							if (hidden) {
-								setHidden(hidden)
-							}else{
-								setHidden(!hidden)
-							}
-						}}>Enviar!</Button>
-					<ButtonHolderStyle>
-						<Show isHidden={hidden} phrase={phrase}/>
-					</ButtonHolderStyle>
-				</PhraseFormStyle>
-				<PhraseReportStyle>
-					<ShowPhrase isHidden={hidden} phrase={phrase?.Mensagem}/>
-					<ShowReport isHidden={hidden} phrase={phrase?.Frase}/>
-				</PhraseReportStyle>
-
-			</Wrapper>
-
-				
-		</>
-	);
+  const [phrase, setPhrase] = useState<IPhrase>();
+  const [hidden, setHidden] = useState(false);
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit: SubmitHandler<IForm> = async (data: any) => {
+    await api.post("/phrase/polarity", data).then((Response: any) => {
+      setPhrase(Response.data.proccess);
+    });
+  };
+  return (
+    <>
+      <Wrapper>
+        <PhraseFormStyle ref={formRef} onSubmit={handleSubmit}>
+          <Input name="phrase" placeholder="Frase" />
+          <Button
+            type="submit"
+            onClick={() => {
+              if (hidden) {
+                setHidden(hidden);
+              } else {
+                setHidden(!hidden);
+              }
+            }}
+          >
+            Enviar!
+          </Button>
+          <ButtonHolderStyle>
+            <Show isHidden={hidden} phrase={phrase} />
+          </ButtonHolderStyle>
+        </PhraseFormStyle>
+        <PhraseReportStyle>
+          <ShowPhrase isHidden={hidden} phrase={phrase?.Mensagem} />
+          <ShowReport isHidden={hidden} phrase={phrase?.Frase} />
+        </PhraseReportStyle>
+      </Wrapper>
+    </>
+  );
 }
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,19 +1,18 @@
-import { FC } from 'react'
-import IPage from '../../interfaces/IPage';
-import PhraseForm from '../MyForm'
-import HomePageStyle from '../styles/HomePageStyle';
-import Navbar from '../Navbar';
-import MainH1 from '../styles/H1WithStyle'
+import { FC } from "react";
+import IPage from "../../interfaces/IPage";
+import PhraseForm from "../MyForm";
+import HomePageStyle from "../styles/HomePageStyle";
+import Navbar from "../Navbar";
+import MainH1 from "../styles/H1WithStyle";
 
-const HomePage:FC<IPage> = () => {
-    return(
-        <HomePageStyle>
-            <Navbar/>
-            <MainH1 value={"<NLP Brasil/>"}/> 
-            <PhraseForm/>
-        </HomePageStyle>
-    )
-    
+const HomePage: FC<IPage> = () => {
+  return (
+    <HomePageStyle>
+      <Navbar />
+      <MainH1 value={"<NLP Brasil/>"} />
+      <PhraseForm />
+    </HomePageStyle>
+  );
 };
 
-export default HomePage
+export default HomePage;
